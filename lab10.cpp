6e4c506a1a81795bab42295404b8e867b9157486
sashaaero <sashaaero@yandex.ru>
2016-11-01 20:08:55 +0300

    lab10 close to complete

--- lab10.cpp
+++ lab10.cpp
@@ -1,13 +1,14 @@
 #include <iostream>
 #include <vector>
+#include <string>
 
 using std::string;
 using std::vector;
 
 class Student{
 private:
-    Student* _prev;
-    Student* _next;
+    Student* _prev = nullptr;
+    Student* _next = nullptr;
 
 public:
     string name;
@@ -21,6 +22,16 @@ public:
         return (this->name == student.name) && (this->lastname == student.lastname);
     }
 
+    void operator = (const Student& student) {
+        name = student.name;
+        lastname = student.lastname;
+    }
+
+    friend std::ostream& operator << (std::ostream& os, const Student& student) {
+        os << student.name << " " << student.lastname;
+        return os;
+    }
+
     void setPrev(Student* student) {
         _prev = student;
     }
@@ -29,12 +40,12 @@ public:
         _next = student;
     }
 
-    Student prev() {
-        return *this->_prev;
+    Student* prev() {
+        return this->_prev;
     }
 
-    Student next() {
-        return *this->_next;
+    Student* next() {
+        return this->_next;
     }
 
 };
@@ -43,24 +54,95 @@ template <typename T> class MyList {
 private:
     T* _begin;
     T* _end;
-    int _size;
+    size_t _size;
 
 public:
     MyList() : _begin(nullptr), _end(nullptr), _size(0){}
 
     MyList(vector<T> objects) : MyList() {
-        T* prev;
-        for (T t: objects) {
-            _size++;
-            if (_begin == nullptr) {
-                _begin = &t;
-                prev = &t;
+        T* prev = nullptr;
+        for (auto t = objects.begin(); t != objects.end(); t++) {
+            if (_size == 0) {
+                _begin = &*t;
+                prev = &*t;
             } else {
-                prev->setNext(&t);
-                t.setPrev(prev);
-                _end = prev = &t;
+                prev->setNext(&*t);
+                (*t).setPrev(prev);
+                _end = &*t;
+                prev = &*t;
+            }
+            _size++;
+        }
+    }
+
+    size_t size() const {
+        return _size;
+    }
+
+    void push_back(T& t) {
+        if (_size == 0) {
+            _begin = &t;
+            _end = &t;
+        } else {
+            _end->setNext(&t);
+            t.setPrev(_end);
+            _end = &t;
+        }
+        _size++;
+    }
+
+    T pop_back() {
+        T* ret = _end;
+        _end = _end->prev();
+        _end->setNext(nullptr);
+        return *ret;
+    }
+
+    friend std::ostream& operator << (std::ostream& os, MyList<T>& list) {
+        for (size_t i = 0; i < list.size(); i++) {
+            os << list[i] << std::endl;
+        }
+        return os;
+    }
+
+    T operator [] (size_t index) const {
+        if (index >= _size) {
+            return *_end;
+        }
+        if (index == 0) {
+            return *_begin;
+        }
+        T* curr = _begin;
+        for (size_t i = 0; i != index; i++) {
+            curr = curr->next();
+        }
+        return *curr;
+    }
+
+    MyList<T> operator && (const MyList<T>& another) const {
+        MyList<T> list;
+        T tmp;
+        for (size_t i = 0; i < _size; i++) {
+            tmp = (*this)[i];
+            list.push_back(tmp);
+        }
+
+        for (size_t i = 0; i < another.size(); i++) {
+            tmp = another[i];
+            if (list.get(tmp) == -1) {
+                list.push_back(tmp);
             }
         }
+        return list;
+    }
+
+    int get(T& t) {
+        for (int i = 0; i < _size; i++) {
+            if ((*this)[i] == t) {
+                return i;
+            }
+        }
+        return -1;
     }
 
     T begin() {
@@ -73,8 +155,43 @@ public:
 };
 
 void lab10(){
-    vector<Student> students = {Student("Leonid", "Abuzin"), Student("Vladislav", "Anikanov")};
-    MyList<Student> myList(students);
-    std::cout << myList.begin().name;
-    std::cout << myList.end().name;
+    vector<Student> kr = {
+        Student("Leonid", "Abuzin"),
+        Student("Vladislav", "Anikanov"),
+        Student("Vera", "Bezrukavaya"),
+        Student("Gennagiy", "Vasilyev"),
+        Student("Ilya", "Vasilyev"),
+        Student("Danila", "Vorontsov"),
+        Student("Anton", "Getman"),
+        Student("Ilya", "Grischenko"),
+        Student("Evgeniy", "Dominov"),
+        Student("Daniil", "Evseev"),
+        Student("Ivan", "Zhitnikov"),
+        Student("Oleg", "Zaplatin"),
+        Student("Alexander", "Tischenko"),
+        Student("Yan", "Tsvitchenko")
+    };
+
+    vector<Student> lab = {
+        Student("Leonid", "Abuzin"),
+        Student("Vladislav", "Anikanov"),
+        Student("Gennagiy", "Vasilyev"),
+        Student("Maxim", "Demchuk"),
+        Student("Ivan", "Zhitnikov"),
+        Student("Alexander", "Tischenko")
+    };
+
+    MyList<Student> krList(kr);
+    MyList<Student> labList(lab);
+    MyList<Student> goodGuys = krList && labList;
+
+//    std::cout << "Сдавшие ТЗ" << std::endl;
+//    std::cout << krList << std::endl;
+//
+//    std::cout << "Сдающие лабы" << std::endl;
+//    std::cout << labList << std::endl;
+//
+//    std::cout << "Молодцы" << std::endl;
+//    std::cout << goodGuys;
+
 };
